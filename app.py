2e1992580198d61938318412f2a3844c8e837bc5
Zoraiz <mnasi098@uottawa.ca>
2025-04-06 17:50:46 -0400

    remade the index file to my own ui

--- app.py
+++ app.py
@@ -16,25 +16,25 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST','GET'])
 def hello():
-    rest_endpoint="http://" + os.environ["VOTING_API_SERVICE_HOST"] + ":" + os.environ["VOTING_API_SERVICE_PORT"]
-    voter_id = request.cookies.get('voter_id')
-    if not voter_id:
-        voter_id = hex(random.getrandbits(64))[2:-1]
+#    // rest_endpoint="http://" + os.environ["VOTING_API_SERVICE_HOST"] + ":" + os.environ["VOTING_API_SERVICE_PORT"]
+    # voter_id = request.cookies.get('voter_id')
+    # if not voter_id:
+    #     voter_id = hex(random.getrandbits(64))[2:-1]
 
-    vote = None
-    if request.method == 'POST':
-        vote = request.form['vote']
-        data = json.dumps({'voter_id': voter_id, 'vote': vote})
-        requests.post(url=rest_endpoint + "/vote", data=data)
+    # vote = None
+    # if request.method == 'POST':
+    #     vote = request.form['vote']
+    #     data = json.dumps({'voter_id': voter_id, 'vote': vote})
+    #     requests.post(url=rest_endpoint + "/vote", data=data)
 
     resp = make_response(render_template(
         'index.html',
-        option_a=option_a,
-        option_b=option_b,
-        hostname=hostname,
-        vote=vote,
+        # option_a=option_a,
+        # option_b=option_b,
+        # hostname=hostname,
+        # vote=vote,
     ))
-    resp.set_cookie('voter_id', voter_id)
+    # resp.set_cookie('voter_id', voter_id)
     return resp
 
 @app.route("/result", methods=['GET'])
@@ -42,12 +42,12 @@ def result():
     return make_response(render_template('result/index.html'))
 
 
-@app.route("/votes", methods=['GET'])
-def votes():
-    rest_endpoint="http://" + os.environ["VOTING_API_SERVICE_HOST"] + ":" + os.environ["VOTING_API_SERVICE_PORT"]
-    response = requests.get(url=rest_endpoint + "/vote")
-    print(response.content)
-    return response.content
+# @app.route("/votes", methods=['GET'])
+# def votes():
+#     rest_endpoint="http://" + os.environ["VOTING_API_SERVICE_HOST"] + ":" + os.environ["VOTING_API_SERVICE_PORT"]
+#     response = requests.get(url=rest_endpoint + "/vote")
+#     print(response.content)
+#     return response.content
 
 @app.route('/templates/<path:path>')
 def send_js(path):
